8f74cbdef2091a6b2150aa8991035f91ed606474
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 04:01:03 +0600

    dic try

--- md2/dic.py
+++ md2/dic.py
@@ -1,1 +1,29 @@
-print("hi from dic.py")
+# # print("hi from dic.py")
+# guest = {"name": "John",
+#  "age": 30, 
+#  "city": "New York"
+#  }
+
+#  print(guest)
+guest = {"name": "John",
+ "age": 30,
+    "city": "New York"
+    }
+print(guest)
+print(guest["name"])
+print(guest["age"])
+print(guest["city"])
+guest["age"]=31
+print(guest)
+guest["country"]="USA"
+print(guest)
+del guest["city"]
+print(guest)
+for key, value in guest.items():
+    print(key, ":", value)
+    print(key)
+    
+
+
+
+
