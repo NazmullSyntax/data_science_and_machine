b4306fdca8542d5212554d7f2cd75510a653a566
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 01:53:24 +0600

    live test one

--- md2/demo.py
+++ md2/demo.py
@@ -1,1 +1,17 @@
-print("hi its my second module for this project")
+# print("hi its my second module for this project")
+# now take a simple exam
+score = float(input("enter your score: "))
+if score<0 or score>100:
+    print("invalid score")
+elif score>=90:
+    print("grade A")
+elif score>=80:
+    print("grade B")
+elif score>=70:
+    print("grade C")
+elif score>=60:
+    print("grade D")
+else:    print("grade F")
+    
+    
+   
