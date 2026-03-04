afc49cf806e9aedba88a997da52b74381ea268ea
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 15:28:03 +0600

    add f

--- md2/data.py
+++ md2/data.py
@@ -1,18 +1,23 @@
-# print("helloworld")
+# # print("helloworld")
+# love = {'python', 'java', 'c++', 'javascript'}
+# love_2 = {'python', 'ruby', 'go'}
+# print(love)
+# love.add('ruby')
+# print(love)
+# love.remove('c++')
+# print(love)
+# love.discard('c++')
+# print(love)
+
+# love.update(love_2)
+# print(love)
+# x = love.pop()
+# print(x)
+# print(love)
+# set union function
 love = {'python', 'java', 'c++', 'javascript'}
 love_2 = {'python', 'ruby', 'go'}
-print(love)
-love.add('ruby')
-print(love)
-love.remove('c++')
-print(love)
-love.discard('c++')
-print(love)
-
-love.update(love_2)
-print(love)
-x = love.pop()
-print(x)
-print(love)
+love_union = love.union(love_2)
+print(love_union)
 
 
