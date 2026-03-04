43da2f7928d8be73b17c942e2d969bbb914d12ab
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 20:14:00 +0600

    i dont know the error

--- md2/fun.py
+++ md2/fun.py
@@ -1,8 +1,16 @@
-# def my_function():
-#     print("Hello, World!")
-# my_function()
-# my_function()
-def my_function(name):
-    print(f"my name is :{name}")
-my_function("Alice")
-my_function("Bob")
+# # def my_function():
+# #     print("Hello, World!")
+# # my_function()
+# # my_function()
+# def my_function(name):
+#     print(f"my name is :{name}")
+# my_function("Alice")
+# my_function("Bob")
+def my_function(first_name, last_name):
+    print(f"my name is :{first_name} {last_name}")
+    x = input("Enter your first_name: ")
+    y = input("Enter your last_name: ")
+    my_function(x, y)
+
+
+
