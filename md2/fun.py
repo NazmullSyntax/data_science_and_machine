a913d692a5b9178de49b965c2adc1d4b85958173
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 20:15:27 +0600

    solve

--- md2/fun.py
+++ md2/fun.py
@@ -1,16 +1,23 @@
-# # def my_function():
-# #     print("Hello, World!")
-# # my_function()
-# # my_function()
-# def my_function(name):
-#     print(f"my name is :{name}")
-# my_function("Alice")
-# my_function("Bob")
+# # # def my_function():
+# # #     print("Hello, World!")
+# # # my_function()
+# # # my_function()
+# # def my_function(name):
+# #     print(f"my name is :{name}")
+# # my_function("Alice")
+# # my_function("Bob")
+# def my_function(first_name, last_name):
+#     print(f"my name is :{first_name} {last_name}")
+#     x = input("Enter your first_name: ")
+#     y = input("Enter your last_name: ")
+#     my_function(x, y)
+
 def my_function(first_name, last_name):
     print(f"my name is :{first_name} {last_name}")
-    x = input("Enter your first_name: ")
-    y = input("Enter your last_name: ")
-    my_function(x, y)
+x = input("Enter your first_name: ")
+y = input("Enter your last_name: ")
+my_function(x, y)
+
 
 
 
