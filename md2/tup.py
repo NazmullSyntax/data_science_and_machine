f01e4116e8906cb44109ed36e0d9ea127d71f688
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 15:45:47 +0600

    add

--- md2/tup.py
+++ md2/tup.py
@@ -1,4 +1,17 @@
 my = ('apple', 'banana', 'cherry')
 print(my)
-print(my[1])
-# my[1] = 'blackcurrant' # This will raise an error because tuples are immutable
+# print(my[1])
+# # my[1] = 'blackcurrant' # This will raise an error because tuples are immutable
+print(len(my))
+print(type(my))
+print(type(my[0]))
+print(type(my[1:3]))
+
+# my_tuple = ('apple', 'banana', 'cherry', 'apple', 'cherry')
+# print(my_tuple)
+# print(my_tuple.count('apple'))
+# print(my_tuple.count('cherry'))
+# print(my type(my_tuple))
+# print(my type(my_tuple[0]))
+# print(my type(my_tuple[1:3]))
+
