a04de27fa386266c177e79bfc0a8ee2529f70c7f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 20:04:33 +0600

    add function

--- md2/fun.py
+++ md2/fun.py
@@ -1,1 +1,8 @@
-print("hi there")
+# def my_function():
+#     print("Hello, World!")
+# my_function()
+# my_function()
+def my_function(name):
+    print(f"my name is :{name}")
+my_function("Alice")
+my_function("Bob")
