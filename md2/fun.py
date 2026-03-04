b919a0f2c73d306702fec1424ea5456ee85df606
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 20:18:22 +0600

    nazmul oj

--- md2/fun.py
+++ md2/fun.py
@@ -17,6 +17,15 @@ def my_function(first_name, last_name):
 x = input("Enter your first_name: ")
 y = input("Enter your last_name: ")
 my_function(x, y)
+def my_registration(name, email, password):
+    print(f"Name: {name}")
+    print(f"Email: {email}")
+    print(f"Password: {password}")
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+my_registration(name, email, password)
+
 
 
 
