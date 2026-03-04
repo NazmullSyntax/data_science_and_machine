ab8324026104db782b3d84aa41c75eea802c8f7f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 15:19:00 +0600

    sss

--- md2/data.py
+++ md2/data.py
@@ -1,1 +1,12 @@
-print("helloworld")
+# print("helloworld")
+love = {'python', 'java', 'c++', 'javascript'}
+print(love)
+love.add('ruby')
+print(love)
+love.remove('c++')
+print(love)
+love.discard('c++')
+print(love)
+love.clear()
+print(love)
+
