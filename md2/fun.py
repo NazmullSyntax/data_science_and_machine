117852d642325b8889fc720e20b82a535b06a6dd
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-03-04 23:54:06 +0600

    add ok

--- md2/fun.py
+++ md2/fun.py
@@ -38,7 +38,9 @@ def cal (a, b):
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 result = cal(num1, num2)
-print(f"The sum of {num1} and {num2} is: {result}")
+# print(f"The sum of {num1} and {num2} is: {result}")
+print(result)
+
 
 
 
